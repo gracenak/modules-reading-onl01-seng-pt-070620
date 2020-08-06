ce2b1b871963d2a85b54196a7a52ea12f0122535
Yuiko Grace Nakano <gracenak@gmail.com>
2020-08-06 03:19:49 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,8 +2,8 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
-  include Dance
-  extend MetaDancing
+  extend FancyDance: :ClassMethods
+  include FancyDance: :InstanceMethods
  
   attr_accessor :name
  
@@ -11,4 +11,5 @@ class Kid
     @name = name
   end
   
+end
 end
